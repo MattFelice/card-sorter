5faa245d68b244fddcf6231266d1553ef6cd959a
Matt felice <matthew.marino.felice+gitlab@gmail.com>
2020-11-21 22:36:33 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './assets/main.css';
+
+function App() {
+  return (
+    <div className="App container mx-auto">
+      <div className="">hello world!</div>
+    </div>
+  );
+}
+
+export default App;
